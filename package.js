6c7ac329c5a33d680b2a978f3fb918a8ab4574cc
Abigail Watson <awatson1978@gmail.com>
2014-11-27 02:34:59 -0500

    forgot to check in borders.less and fonts.less; removing from package reference; bump to v1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "UI vocabulary for ClinicalFramework.",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "http://github.com/awatson1978/clinical-ui-vocabulary.git",
   name: "clinical:ui-vocabulary"
 });
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use('ian:bootstrap-3');
 
   api.addFiles('anchoring.less', 'client');
-  api.addFiles('borders.less', 'client');
+  //api.addFiles('borders.less', 'client');
   api.addFiles('colors.less', 'client');
-  api.addFiles('fonts.less', 'client');
+  //api.addFiles('fonts.less', 'client');
   api.addFiles('haptics.less', 'client');
   api.addFiles('padding.less', 'client');
   api.addFiles('pages.less', 'client');
